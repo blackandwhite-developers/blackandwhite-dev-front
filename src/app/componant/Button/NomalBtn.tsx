4ead9a3952d5606f2d540db0ca2b5a10c3e60072
taeyoung0001 <her020212@naver.com>
2024-11-05 11:09:44 +0900

    버튼 준비

--- src/app/componant/Button/NomalBtn.tsx
+++ src/app/componant/Button/NomalBtn.tsx
@@ -1,3 +1,8 @@
-const NomalBtn = () => {};
+import React from "react";
+import styles from "./Button.module.scss"; // CSS 모듈을 import
+
+const NomalBtn = () => {
+  <button>시작하기</button>;
+};
 
 export default NomalBtn;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import NomalBtn from "./componant/Button/NomalBtn";
 
 export default function Home() {
   return <React.Fragment></React.Fragment>;
