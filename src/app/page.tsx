6d3152e5a88aaf0c629b4ea6d8c8c63c558b2c77
hyunnam-cho <wiwi154@naver.com>
2024-11-05 17:47:56 +0900

    [조현남] productCard component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,22 @@
 import React from "react";
+// import ProductCard from "../components/ProductCard/ProductCard";
+import ProductDetailCard from "@/components/ProductDetailCard/ProductDetailCard";
 
 export default function Home() {
-  return <React.Fragment></React.Fragment>;
+  return (
+    <>
+      <ProductDetailCard
+        imageUrl={"https://dummyimage.com/300"}
+        label={"호텔"}
+        title={"김포 마리나베이 호텔"}
+        infomation={{
+          parlorInfomation: "기준 2인 (최대 3인)",
+          checkInInfomation: "16:00",
+          checkOutInfomatio: "22:00",
+          lodgePrice: 75000,
+          roomCount: 1,
+        }}
+      />
+    </>
+  );
 }
