f42285e09fbd989ebd8582c8a123448a7f120c3b
peppone-choi <peppone.choi@gmail.com>
2024-11-05 17:47:04 +0900

    [최병호] 체크박스 및 아코디언 제작

--- src/app/components/checkbox/slide/SlideCheckbox.tsx
+++ src/app/components/checkbox/slide/SlideCheckbox.tsx
@@ -1,35 +1,28 @@
 "use client";
 import React from "react";
-import styles from "./Checkbox.module.scss";
+import styles from "./SlideCheckbox.module.scss";
 import cn from "classnames/bind";
-import CheckIcon from "../icon/CheckIcon";
+
 const cx = cn.bind(styles);
 
-type CheckBoxProps = {
+type SlideCheckBoxProps = {
   label: string;
   checked?: boolean;
-  isTransparent?: boolean;
   isLabelBold?: boolean;
   isLabelGray?: boolean;
 };
 
-export default function CheckBox(props: CheckBoxProps) {
-  const { label, checked = false, isTransparent = false, isLabelBold = false, isLabelGray = false } = props;
+export default function SlideCheckbox(props: SlideCheckBoxProps) {
+  const { label, checked = false, isLabelBold = false, isLabelGray = false } = props;
   const [check, setCheck] = React.useState(checked);
   const handleChange = () => {
     setCheck((prev) => !prev);
   };
   return (
     <label className={cx("Container")}>
-      <div
-        className={cx("Checkmark", {
-          Checked: check,
-          Transparent: isTransparent,
-        })}
-      >
-        <CheckIcon checked={check} isTransparent={isTransparent} shadow={true} />
-      </div>
       <span className={cx("Label", { isLabelBold, isLabelGray })}>{label}</span>
+      <div className={cx("Slide", { Checked: check })} />
+
       <input type="checkbox" checked={check} hidden onChange={handleChange} />
     </label>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
 import React from "react";
-import CheckBox from "./components/checkbox/Checkbox";
-
+import DefaultCheckBox from "./components/checkbox/default/DefaultCheckbox";
+import SlideCheckbox from "./components/checkbox/slide/SlideCheckbox";
+import DefaultAccordion from "./components/arcodian/default/DefaultAccordion";
+import "./page.scss";
 export default function Home() {
   return (
     <React.Fragment>
-      <CheckBox label="이용약관에 동의합니다." />
+      <DefaultCheckBox label="이용약관에 동의합니다." />
+      <SlideCheckbox label={"ON / OFF"} />
+      <DefaultAccordion title="아코디언 테스트">으하하</DefaultAccordion>
+      <DefaultAccordion
+        isHeaderTransparent
+        title={
+          <React.Fragment>
+            <DefaultCheckBox label="이용약관에 동의합니다." />
+          </React.Fragment>
+        }
+      >
+        으하하2
+      </DefaultAccordion>
     </React.Fragment>
   );
 }
