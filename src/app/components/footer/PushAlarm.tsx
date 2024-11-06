f453fe6c261f0177027a2fd3c0ac0da5ffe104a8
cherry2407 <codus7958@gmail.com>
2024-11-06 16:25:31 +0900

    241106 이미지 아이콘 파일명 수정 및 푸쉬알람 이미지 경로 수정

--- src/app/components/footer/PushAlarm.tsx
+++ src/app/components/footer/PushAlarm.tsx
@@ -9,10 +9,17 @@ const PushAlarm = () => {
         <p>3분 전</p>
       </div>
       <div className="main-container">
-        <Image src="/void" alt="pushalarm image" width={200} height={200} />
+        <Image
+          src="/public/icon-asset/logo-icon.png"
+          alt="pushalarm image"
+          width={200}
+          height={200}
+        />
         <div className="push-text">
           <p className="title">(광고) 푸쉬알람</p>
-          <p className="content">푸쉬 알람과 관련된 프로모션 내용에 대한 텍스트가 들어갑니다.</p>
+          <p className="content">
+            푸쉬 알람과 관련된 프로모션 내용에 대한 텍스트가 들어갑니다.
+          </p>
         </div>
       </div>
     </div>
