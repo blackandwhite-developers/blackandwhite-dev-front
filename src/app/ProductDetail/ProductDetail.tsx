2e0551674a7363550b534d8510270e8aa282cd90
ggaong <gain223@naver.com>
2024-11-06 12:26:08 +0900

    ProductDetail 페이지 생성중

--- src/app/ProductDetail/ProductDetail.tsx
+++ src/app/ProductDetail/ProductDetail.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 import cn from "classnames/bind";
 import styles from "./ProductDetail.module.scss";
 import {
@@ -6,56 +7,81 @@ import {
     IoIosHeartEmpty,
     IoIosStarOutline,
 } from "react-icons/io";
-import { IoLocationOutline, IoCalendarClearOutline } from "react-icons/io5";
+import { IoLocationOutline } from "react-icons/io5";
 // import { BsCart2 } from "react-icons/bs";
-import { CiUser } from "react-icons/ci";
 import Header from "../components/Header/Header";
+import { DateBtn } from "../components/Button/DateBtn";
+import { MemberBtn } from "../components/Button/MemberBtn";
 
 const cx = cn.bind(styles);
 
 const ProductDetail = () => {
     const title = "김포 마리나베이 호텔";
     return (
-        <div>
-            {/* <IoIosArrowBack />
-            김포 마리나베이 호텔
-            <BsCart2 /> */}
+        <div className={cx("ProductDetailWrapper")}>
             <Header title={title} />
-            <div className={cx("ProductImage")}>사진</div>
-            <div className={cx("ProductInform")}>
-                <p>호텔</p>
-                <h1>김포 마리나베이 호텔</h1>
-                <IoIosHeartEmpty />
-                <p>4.0</p>
-                <IoIosStarOutline />
-                <IoIosStarOutline />
-                <IoIosStarOutline />
-                <IoIosStarOutline />
-                <IoIosStarOutline />
-                <p>(1,136)</p>
-                <IoLocationOutline />
-                <p>김포공항역 3분</p>
+            <div className={cx("ProductImage")}>
+                <Image
+                    src="/images/HotelImage1.png"
+                    alt="탭 바 이미지"
+                    width={360}
+                    height={228}
+                />
             </div>
-            <div>
-                객실선택
-                <div>
-                    <IoCalendarClearOutline />
-                    <p>달력</p>
-                    <CiUser />
-                    <p>성인 2명</p>
-                    <div>
-                        <p className={cx("ProductMainImage")}>사진</p>
-                        <h1>프리미엄 트윈</h1>
-                        <p>할인</p>
-                        <div>
-                            <p>객실정보</p>
-                            <p>기준 2인 (최대 3인)</p>
-                            <p>입실/퇴실</p>
-                            <p>입실 16:00 퇴실 11:00</p>
+            <div className={cx("ProductInform")}>
+                <p className={cx("ProductCategory")}>호텔</p>
+                <div className={cx("ProductWrapper")}>
+                    <div className={cx("ProductTitleWrapper")}>
+                        <h1 className={cx("ProductTitle")}>{title}</h1>
+                        <p className={cx("ProductFavorite")}>
+                            <IoIosHeartEmpty />
+                        </p>
+                    </div>
+                    <div className={cx("ProductRating")}>
+                        <p className={cx("ProductRatingText")}>4.0</p>
+                        <div className={cx("ProductStarRating")}>
+                            <IoIosStarOutline />
+                            <IoIosStarOutline />
+                            <IoIosStarOutline />
+                            <IoIosStarOutline />
+                            <IoIosStarOutline />
                         </div>
-                        <p>1개남음</p>
-                        <p>75,000원</p>
-                        <button>객실 예약하기</button>
+                        <p className={cx("ProductVoteTotal")}>(1,136)</p>
+                    </div>
+                    <div className={cx("ProductLocation")}>
+                        <p className={cx("ProductLocationIcon")}>
+                            <IoLocationOutline />
+                        </p>
+                        <p>김포공항역 3분</p>
+                    </div>
+                </div>
+
+                <div className={cx("ProductSelectWrapper")}>
+                    <div className={cx("ProductCategoryBadge")}>
+                        <p>객실 선택</p>
+                        <p>후기</p>
+                    </div>
+
+                    <div>
+                        <div className={cx("ReservationSelectBtn")}>
+                            <DateBtn label={""} />
+                            <MemberBtn label={""} />
+                        </div>
+
+                        <div>
+                            <p className={cx("ProductMainImage")}>사진</p>
+                            <h1>프리미엄 트윈</h1>
+                            <p>할인</p>
+                            <div>
+                                <p>객실정보</p>
+                                <p>기준 2인 (최대 3인)</p>
+                                <p>입실/퇴실</p>
+                                <p>입실 16:00 퇴실 11:00</p>
+                            </div>
+                            <p>1개남음</p>
+                            <p>75,000원</p>
+                            <button>객실 예약하기</button>
+                        </div>
                     </div>
                 </div>
             </div>
