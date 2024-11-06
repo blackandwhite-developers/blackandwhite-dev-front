987bb3cf6f6372c7d376ea0f857aaed96667943a
euneunsol <eunsol1882@gmail.com>
2024-11-06 09:36:33 +0900

    로그인 마크업중

--- src/app/Login/Login.tsx
+++ src/app/Login/Login.tsx
@@ -1,21 +1,9 @@
 "use client"
 import React from 'react';
-import { LuEye, LuEyeOff } from "react-icons/lu";
 import styles from './Login.module.scss';
 import TextInput from "../components/TextInput/TextInput";
-import ColorBorderTextInput from '../components/ColorBorderTextInput/ColorBorderTextInput';
-import PhoneConfirm from '../components/PhoneConfirm/PhoneConfirm';
-import SearchBar from '../components/SearchBar/SearchBar';
 
 const Login = () => {
-    console.log(styles);
-    const handleResend = () => {
-        alert('인증 번호를 재전송했습니다!');
-    };
-
-    const handleSearch = (query: string) => {
-        alert(`검색어: ${query}`);
-    };
 
     return (
         <div className={styles.wrapper}>
@@ -24,24 +12,13 @@ const Login = () => {
                 <TextInput type="text" placeholder="이메일" />
                 <TextInput type="password" placeholder="비밀번호" showToggle />
 
-
-                <ColorBorderTextInput
-                    type="text"
-                    placeholder="예약자 이름"
-                    borderColor="#8728FF" 
-                />
-                <ColorBorderTextInput
-                    type="text"
-                    placeholder="010-1234-5678"
-                    borderColor="#E5E5E5" 
-                />
-
-                <PhoneConfirm onResend={handleResend} />
-
-                <SearchBar onSearch={handleSearch} />
-
                 <input type="button" value="로그인" />
-                <a href="./Sign">회원가입</a>
+                <div className={styles.authLinks}>
+                    <a href="./FindID">아이디찾기</a>
+                    <a href="./FindPW">비밀번호 찾기</a>
+                    <a href="./Sign">회원가입</a>
+                </div>
+                
             </div>
         </div>
     );
---
+++ src/app/SignUp/SignUp.tsx
@@ -0,0 +1,12 @@
+"use client"
+import React from "react";
+import style from "./SignUp.module.scss";
+
+const SignUp = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+export default SignUp
