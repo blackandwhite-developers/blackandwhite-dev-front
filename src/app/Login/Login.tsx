4365f8ac98528158f7e41aaace44b433f37db1ab
euneunsol <eunsol1882@gmail.com>
2024-11-06 11:29:10 +0900

    input 컴포넌트 수정중

--- src/app/Login/Login.tsx
+++ src/app/Login/Login.tsx
@@ -10,8 +10,8 @@ const Login = () => {
         <div className={styles.wrapper}>
             <div className={styles.container}>
                 <h1>로그인</h1>
-                <TextInput type="text" placeholder="이메일" />
-                <TextInput type="password" placeholder="비밀번호" showToggle />
+                <TextInput type="text" placeholder="이메일" className='textInput' />
+                <TextInput type="password" placeholder="비밀번호" showToggle className='textInput' />
 
                 <AbleBtn label="로그인" /> 
                 <div className={styles.authLinks}>
