6a9884a3ac5c8a802bb43175593f465ebc69c5df
ggaong <gain223@naver.com>
2024-11-07 15:06:45 +0900

    header 컴포넌트 title 옵셔널 처리

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -6,19 +6,19 @@ import { IoIosArrowBack } from "react-icons/io";
 const cx = cn.bind(styles);
 
 type HeaderProps = {
-  title: string;
+    title?: string;
 };
 
 const Header = ({ title }: HeaderProps) => {
-  return (
-    <div className={cx("Header")}>
-      <a href="" className={cx("BackIcon")}>
-        <IoIosArrowBack />
-      </a>
+    return (
+        <div className={cx("Header")}>
+            <a href="" className={cx("BackIcon")}>
+                <IoIosArrowBack />
+            </a>
 
-      <div className={cx("HeaderTitle")}>{title}</div>
-    </div>
-  );
+            <div className={cx("HeaderTitle")}>{title}</div>
+        </div>
+    );
 };
 
 export default Header;
