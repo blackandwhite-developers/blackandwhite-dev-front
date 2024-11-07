799bbadb9e8f1a3dacc200e802398d824f3b4ac9
peppone-choi <peppone.choi@gmail.com>
2024-11-07 13:48:37 +0900

    [최병호] recoil 및 recoil-persist 설치

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { MockProvider } from "@/mockProvider";
+import RecoilProvider from "@/recoilProvider";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,7 +16,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <MockProvider>{children}</MockProvider>
+        <MockProvider>
+          <RecoilProvider>{children}</RecoilProvider>
+        </MockProvider>
       </body>
     </html>
   );
