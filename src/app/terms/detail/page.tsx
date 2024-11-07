7a506bb92cdf83444ec40a405476fe3ee678193f
peppone-choi <peppone.choi@gmail.com>
2024-11-07 17:00:16 +0900

    [최병호] terms 수정

--- src/app/terms/detail/page.tsx
+++ src/app/terms/detail/page.tsx
@@ -1,5 +1,12 @@
-import TermsView from "@/views/terms/TermsView";
+import TermsOfServiceDetailView from "@/views/terms/TermsView";
 
-export default function TermsPage() {
-  return <TermsView />;
+type SearchParams = Promise<{ [key: string]: string | string[] | undefined }>;
+
+export default async function TermsPage(props: { searchParams: SearchParams }) {
+  const searchParams = await props.searchParams;
+  const type = searchParams.type;
+  if (type !== "service" && type !== "location" && type !== "privacy" && type !== "marketing") {
+    throw new Error("Invalid type");
+  }
+  return <TermsOfServiceDetailView type={type} />;
 }
