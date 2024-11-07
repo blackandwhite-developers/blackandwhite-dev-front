d900a33ffa7709b4d0a41931d1c954a51f877bdc
cherry2407 <codus7958@gmail.com>
2024-11-07 09:23:09 +0900

    241107 결제페이지 작업 시작

--- src/app/Payment/Payment.tsx
+++ src/app/Payment/Payment.tsx
@@ -14,10 +14,19 @@ const ReservetionContent = () => {
         </p>
       </div>
       <div className={cx("reservationdate-container")}>
-        <p className={cx("hotelname-title")}>김포 마리나베이 호텔</p>
-        <p className={cx("room-detailcontent")}>
-          디럭스 트윈 (기준 2명/최대 2명)
-        </p>
+        <div className={cx("date-box")}>
+          <p className={cx("check in")}>체크인</p>
+          <p className={cx("date-text")}>2024.11.07(목)</p>
+          <p className={cx("date-time")}>16:00</p>
+        </div>
+        <div className={cx("day-box")}>
+          <p className={cx("day-text")}>1박</p>
+        </div>
+        <div className={cx("date-box")}>
+          <p className={cx("check out")}>체크아웃</p>
+          <p className={cx("date-text")}>2024.11.08(금)</p>
+          <p className={cx("date-time")}>16:00</p>
+        </div>
       </div>
     </div>
   );
