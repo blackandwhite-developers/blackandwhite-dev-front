1e769a5138bb7b124848d5bb2e8d29e7408b1ec8
taeyoung0001 <her020212@naver.com>
2024-11-07 15:58:37 +0900

    그리드 어렵다..

--- src/app/Hotel/Hotel.tsx
+++ src/app/Hotel/Hotel.tsx
@@ -32,6 +32,58 @@ const Hotel = () => {
     },
   ];
 
+  const PopData = [
+    {
+      img: "/categoryImage/HotelImg/pop_01.svg",
+      name: "김포 마리나베이 호텔",
+      rate: "4.0",
+      count: "136",
+      distance: "김포공항역 3분",
+      price: "75,000",
+    },
+    {
+      img: "/categoryImage/HotelImg/pop_02.svg",
+      name: "더블유 에비뉴 김포",
+      rate: "4.0",
+      count: "136",
+      distance: "김포공항역 5분",
+      price: "85,000",
+    },
+    {
+      img: "/categoryImage/HotelImg/pop_03.svg",
+      name: "리벨 닷지 호텔",
+      rate: "4.0",
+      count: "136",
+      distance: "경단역 5분",
+      price: "85,000",
+    },
+    {
+      img: "/categoryImage/HotelImg/pop_04.svg",
+      name: "김포 B hotel",
+      rate: "4.0",
+      count: "136",
+      distance: "김포공항역 10분",
+      price: "55,000",
+    },
+
+    {
+      img: "/categoryImage/HotelImg/pop_05.svg",
+      name: "호텔 Arbo",
+      rate: "4.0",
+      count: "136",
+      distance: "아시아 게임 주 경기장 5분",
+      price: "65,000",
+    },
+    {
+      img: "/categoryImage/HotelImg/pop_06.svg",
+      name: "호텔 더 루티크",
+      rate: "4.0",
+      count: "136",
+      distance: "김포공항역 3분",
+      price: "105,000",
+    },
+  ];
+
   return (
     <div className={cx("container")}>
       <div className={cx("header")}>헤더바 위치</div>
@@ -46,7 +98,7 @@ const Hotel = () => {
           {data.map((a, i) => {
             return (
               <div className={cx("grid-item")}>
-                <img src={data[i].img} alt="motel" />
+                <img src={data[i].img} alt="regoinImg" />
                 <div className={cx("title")}>{data[i].title}</div>
               </div>
             );
@@ -56,6 +108,27 @@ const Hotel = () => {
 
       <div className={cx("popularityRoom")}>
         <h3>지금 핫한 숙소!</h3>
+        <div className={cx("pop-grid-item")}>
+          <img src={PopData[0].img} alt="" />
+          <div className={cx("pop-title")}>{PopData[0].name}</div>
+          <div className={cx("pop-raet-info")}>
+            <div className={cx("pop-rate")}>{PopData[0].rate}</div>
+            <div className={cx("pop-count")}>{PopData[0].count}</div>
+          </div>
+          <div className={cx("pop-distance")}>{PopData[0].distance}</div>
+          <div className={cx("pop-price")}>{PopData[0].price}</div>
+        </div>
+
+        <div className={cx("pop-grid-item")}>
+          <img src={PopData[1].img} alt="" />
+          <div className={cx("pop-title")}>{PopData[1].name}</div>
+          <div className={cx("pop-raet-info")}>
+            <div className={cx("pop-rate")}>{PopData[1].rate}</div>
+            <div className={cx("pop-count")}>{PopData[1].count}</div>
+          </div>
+          <div className={cx("pop-distance")}>{PopData[1].distance}</div>
+          <div className={cx("pop-price")}>{PopData[1].price}</div>
+        </div>
       </div>
       <div className={cx("reservationClosedRoom")}>
         <h3>예약이 빨리 마감되는 숙소!</h3>
