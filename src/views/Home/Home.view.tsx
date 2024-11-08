97455d6313556229ee9d7ff28fef4b5d64c85180
peppone-choi <peppone.choi@gmail.com>
2024-11-08 17:51:52 +0900

    [최병호] 체크박스 수정

--- src/views/Home/Home.view.tsx
+++ src/views/Home/Home.view.tsx
@@ -5,13 +5,16 @@ import cn from "classnames/bind";
 import Header from "@/app/components/Header/Header";
 import { ImageIcon } from "@/app/components/imageIcon/ImageIcon";
 import FooterBar from "@/app/components/footer/FooterBar";
+import DefaultCheckBox from "@/app/components/checkbox/default/DefaultCheckbox";
 const cx = cn.bind(styles);
 
 export default async function HomeView() {
   return (
-    <div className="pageContainer">
+    <div className={cx("pageContainer")}>
       <Header leftIcon={<ImageIcon src={"/home/img_home_logo.svg"} alt={"로고"} width={117} height={21} borderRadius={0} />} rightIcon={<PiBellSimpleLight />} className={cx("Header")} />
-      <div className="content"></div>
+      <div className={cx("content")}>
+        <DefaultCheckBox label="체크박스" />
+      </div>
       <FooterBar />
     </div>
   );
