dae1be71d82b026ae1e3140c4b5b8326acbd37bc
taeyoung0001 <her020212@naver.com>
2024-11-08 17:50:36 +0900

    #118 알림창 완료

--- src/views/Alert/Alert.view.tsx
+++ src/views/Alert/Alert.view.tsx
@@ -10,20 +10,20 @@ const AlertPageView = () => {
     {
       img: "/images/alert/alram_present.svg",
       title: "선물이 도착했어요",
-      content: "2023/1/1일까지 사용할 수 있는 쿠폰이 도착",
+      content: "2023/1/1일까지 사용할 수 있는 쿠폰이 도착했습니다.",
       time: "3분",
     },
     {
       img: "/images/alert/alram_point.svg",
       title: "혜택 추천",
-      content: "포인트를 받을 수 있는 기간이 얼마 남지 않았어요",
+      content: "포인트를 받을 수 있는 기간이 얼마 남지 않았어요.",
       time: "9시간",
     },
     {
       img: "/images/alert/alram_point.svg",
       title: "포인트 적립",
       content: "포인트 적립 이벤트 기간을 확인해보세요!",
-      time: "10시간 전",
+      time: "10시간",
     },
   ];
   return (
@@ -32,7 +32,7 @@ const AlertPageView = () => {
       <div className={cx("alert-container")}>
         {Data.map((a, i) => {
           return (
-            <div className={cx("alert-card")}>
+            <div className={cx("alert-card")} key={i}>
               <img src={a.img} alt="" />
               <div className={cx("alert-card-about")}>
                 <p className={cx("title")}>{a.title}</p>
