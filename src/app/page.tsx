63b03bbcc5aecc243d73e0a36fef3db6300c262f
euneunsol <eunsol1882@gmail.com>
2024-11-13 16:05:43 +0900

    [은은솔] splash, 온보딩 완료

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import HomeView from "@/views/Home/Home.view";
+import OnboardingView from "@/views/Onboading/Onboarding.view";
+import SplashView from "@/views/Splash/Splash.view";
 
 export default async function Home() {
-  return <HomeView />;
+  return <SplashView/>
 }
