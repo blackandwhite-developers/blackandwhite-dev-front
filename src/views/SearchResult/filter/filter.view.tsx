8cea805854581fe92eee8817cdf12f0aa8b55570
taeyoung0001 <her020212@naver.com>
2024-11-14 09:41:41 +0900

    재부팅 하기전 저장

--- src/views/SearchResult/filter/filter.view.tsx
+++ src/views/SearchResult/filter/filter.view.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 import styles from "./filter.view.module.scss";
 import cn from "classnames/bind";
+import Header from "@/app/components/Header/Header";
+import { MdClose } from "react-icons/md";
+import DefaultCheckBox from "@/app/components/checkbox/default/DefaultCheckbox";
 
 const cx = cn.bind(styles);
 
-const Filter = () => {
-  return <div> 필터 페이지임</div>;
+const FilterPageView = () => {
+  return (
+    <div>
+      <div>
+        <Header title={"날짜 선택"} leftIcon={<MdClose />} rightIcon={"초기화"} />
+      </div>
+      <div className={cx("room-type")}>
+        <h3>숙소 유형</h3>
+      </div>
+    </div>
+  );
 };
 
-export default Filter;
+export default FilterPageView;
