80248bf8903f0e2948040a830134209a10ac4eff
ggaong <gain223@naver.com>
2024-11-15 14:55:52 +0900

    닉네임 랜덤 생성 기능 추가

--- src/views/UserSelect/Nickname/Nickname.tsx
+++ src/views/UserSelect/Nickname/Nickname.tsx
@@ -1,21 +1,47 @@
 "use client";
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 import cn from "classnames/bind";
 import styles from "./Nickname.module.scss";
 import Header from "@/app/components/Header/Header";
 import { FaAngleLeft } from "react-icons/fa6";
 import { AbleBtn } from "@/app/components/Button/AbleBtn";
+import { DisableBtn } from "@/app/components/Button/DisableBtn";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
+
 const cx = cn.bind(styles);
 
 const Nickname = () => {
-    /** 뒤로가기 */
     const router = useRouter();
+    /** 뒤로가기 */
     const handleGoBack = () => {
         router.back();
     };
+
+    const [nickname, setNickname] = useState("");
+
+    const generateRandomNickname = () => {
+        const characters =
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        let randomNickname = "";
+        for (let i = 0; i < 8; i++) {
+            randomNickname += characters.charAt(
+                Math.floor(Math.random() * characters.length)
+            );
+        }
+        return randomNickname;
+    };
+
+    useEffect(() => {
+        const randomNickname = generateRandomNickname();
+        setNickname(randomNickname);
+    }, []);
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setNickname(e.target.value);
+    };
+
     return (
         <div className={cx("SelectNicknameWrapper")}>
             <Header leftIcon={<FaAngleLeft onClick={handleGoBack} />} />
@@ -32,19 +58,22 @@ const Nickname = () => {
                     id="nickname"
                     type="text"
                     className={cx("NicknameInput")}
+                    value={nickname}
+                    onChange={handleInputChange}
                 />
             </div>
 
             <div className={cx("SelectGenderNextBtn")}>
-                {" "}
-                <Link href="/login">
-                    <AbleBtn label={"다음"} />{" "}
-                </Link>
+                {nickname ? (
+                    <Link href="/login">
+                        <AbleBtn label={"다음"} />
+                    </Link>
+                ) : (
+                    <DisableBtn label={"다음"} />
+                )}
             </div>
         </div>
     );
 };
 
 export default Nickname;
-
-//에러문구는 아직 미작업 (백엔드 완료되면 작업!)
