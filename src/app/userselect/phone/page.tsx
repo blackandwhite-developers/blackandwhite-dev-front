b86af177e7b53ca1be9c1109ccb8b15664febadf
peppone-choi <peppone.choi@gmail.com>
2024-11-20 11:11:08 +0900

    [최병호] 유저 회원가입 로직 수정

--- src/app/userselect/phone/page.tsx
+++ src/app/userselect/phone/page.tsx
@@ -3,22 +3,22 @@ import PhoneView from "@/views/UserSelect/Phone/Phone";
 import { useRouter } from "next/navigation";
 
 export default function Phone() {
-    const router = useRouter();
-    const signUpData = localStorage.getItem("signUpData");
-    const data = signUpData ? JSON.parse(signUpData) : null;
-    if (!data) {
-        router.push("/signup");
-    }
-    const phoneFunc = async (phone: string) => {
-        const newData = {
-            ...data,
-            profile: {
-                ...data.profile,
-                phone: phone,
-            },
-        };
-        localStorage.setItem("signUpData", JSON.stringify(newData));
-        router.push("/userselect/birth");
+  const router = useRouter();
+  const signUpData = localStorage.getItem("signUpData");
+  const data = signUpData ? JSON.parse(signUpData) : null;
+  if (!data) {
+    router.push("/signup");
+  }
+  const phoneFunc = async (phone: string) => {
+    const newData = {
+      ...data,
+      profile: {
+        ...data.profile,
+        phone: phone,
+      },
     };
-    return <PhoneView PhoneFunc={phoneFunc} />;
+    localStorage.setItem("signUpData", JSON.stringify(newData));
+    router.push("/userselect/birth");
+  };
+  return <PhoneView PhoneFunc={phoneFunc} />;
 }
