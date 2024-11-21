08b45487ddd544b4663b6e64efa83c604c42fc18
euneunsol <eunsol1882@gmail.com>
2024-11-21 10:32:42 +0900

    [은은솔] Home.view 수정

--- src/views/Home/Home.view.tsx
+++ src/views/Home/Home.view.tsx
@@ -1,4 +1,4 @@
-"use client";
+"use client"
 import React, { useState, useEffect } from "react";
 import cn from "classnames/bind";
 import styles from "./Home.view.module.scss";
@@ -19,8 +19,8 @@ export interface HomeviewProps {
 const Homeview = (props: HomeviewProps) => {
   const { category, resentView } = props;
   const [src, setSrc] = useState("/home/home_banner_desktop.png");
-
   const [isAlarm, setIsAlarm] = useState(false);
+
   useEffect(() => {
     const handleResize = () => {
       if (window.innerWidth < 768) {
@@ -32,7 +32,10 @@ const Homeview = (props: HomeviewProps) => {
     window.addEventListener("resize", handleResize);
     handleResize();
 
-
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   useEffect(() => {
     const eventSource = new EventSource("http://localhost:4000/api/event");
@@ -68,62 +71,38 @@ const Homeview = (props: HomeviewProps) => {
       <main className={cx("main-container")}>
         <SearchBar />
         <div className={cx("grid-container")}>
-          {category.map((a) => {
-            return (
-              <Link href={`/home/list/${a.path}`} key={a.id}>
-                <MainCategory
-                  categoryName={a.title}
-                  categoryIcon={`Http://${a.thumbnail}.svg`}
-                  categoryKoreanName={a.title}
-                />
-              </Link>
-            );
-          })}
+          {category.map((a) => (
+            <Link href={`/home/list/${a.path}`} key={a.id}>
+              <MainCategory
+                categoryName={a.title}
+                categoryIcon={`http://${a.thumbnail}.svg`}
+                categoryKoreanName={a.title}
+              />
+            </Link>
+          ))}
         </div>
-                <Link href={"/alert"}>
-                    <div className={cx("bell")}>
-                        <PiBellSimpleThin
-                            style={{ width: "100%", height: "100%" }}
-                        />
-                    </div>
-                </Link>
-            </header>
         <div className={cx("currentList")}>
           <h4>최근 본 숙소</h4>
           <div className={cx("list-container")}>
             {resentView?.map((item) => (
               <div className={cx("list-item")} key={item.id}>
                 <div className={cx("list-image")}>
-                  <img src={item.thumbnail} alt={item.title} />
+                  <img src={item.image} alt={item.name} />
                 </div>
-                <div className={cx("banner")}>
-                    <img src={src} alt="" />
+                <div className={cx("list-title")}>
+                  <p>{item.name}</p>
                 </div>
-                <div className={cx("currentList")}>
-                    <h4>최근 본 숙소</h4>
-                    <div className={cx("list-container")}>
-                        {currentDate.map((item) => (
-                            <div className={cx("list-item")} key={item.id}>
-                                <div className={cx("list-image")}>
-                                    <img
-                                        src={item.thumbnail}
-                                        alt={item.title}
-                                    />
-                                </div>
-                                <div className={cx("list-title")}>
-                                    <p>{item.title}</p>
-                                </div>
-                                <div className={cx("list-price")}>
-                                    <p>{item.price.toLocaleString()}원</p>
-                                </div>
-                            </div>
-                        ))}
-                    </div>
+                <div className={cx("list-price")}>
+                  <p>{item.price.toLocaleString()}원</p>
                 </div>
-            </main>
-            <FooterBar />
+              </div>
+            ))}
+          </div>
         </div>
-    );
+      </main>
+      <FooterBar />
+    </div>
+  );
 };
 
 export default Homeview;
