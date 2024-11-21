caa0b95ecd7a11b1f320da53dae02124073ca12c
peppone-choi <peppone.choi@gmail.com>
2024-11-21 13:10:23 +0900

    [최병호] 로그인 수정

--- src/app/mypage/myinfo/page.tsx
+++ src/app/mypage/myinfo/page.tsx
@@ -23,6 +23,8 @@ const Mypage = () => {
       isAuth: false,
       user: null,
     });
+    document.cookie = "accessToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    document.cookie = "refreshToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
   };
 
   const logoutFn = () => {
