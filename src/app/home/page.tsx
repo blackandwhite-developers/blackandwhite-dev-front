0297ae1e1a054135d4d692a49f68b2200dfaab32
taeyoung0001 <her020212@naver.com>
2024-11-21 10:35:00 +0900

    장바구니 반응형 중간 저장

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,19 +1,19 @@
-import React from "react";
+// import React from "react";
 
-import Homeview from "@/views/Home/Home.view";
-import { categoryService } from "@/api/services";
+// import Homeview from "@/views/Home/Home.view";
+// import { categoryService } from "@/api/services";
 
-const getAllCategories = async () => {
-  const res = await categoryService.getAllCategories();
-  const category = res.results;
-  return { category };
-};
+// const getAllCategories = async () => {
+//   const res = await categoryService.getAllCategories();
+//   const category = res.results;
+//   return { category };
+// };
 
-const HomePage = async () => {
-  const res = await getAllCategories();
-  const category = res.category;
+// const HomePage = async () => {
+//   const res = await getAllCategories();
+//   const category = res.category;
 
-  return <Homeview category={category} />;
-};
+//   return <Homeview category={category} />;
+// };
 
-export default HomePage;
+// export default HomePage;
