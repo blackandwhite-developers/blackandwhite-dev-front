16edf75d367666a5b04d03f2c602d2b661183ffb
ggaong <gain223@naver.com>
2024-11-22 13:44:10 +0900

    마이페이지 인증 후 화면 마크업

--- src/views/Mypage/CertificationComplete/CertificationComplete.view.tsx
+++ src/views/Mypage/CertificationComplete/CertificationComplete.view.tsx
@@ -2,22 +2,20 @@
 
 import React, { useState } from "react";
 import cn from "classnames/bind";
-import styles from "./MyInfo.module.scss";
+import styles from "./CertificationComplete.module.scss";
 import Image from "next/image";
 import { FaAngleLeft } from "react-icons/fa6";
 import { MdOutlinePhotoCamera } from "react-icons/md";
 import Header from "@/components/Header/Header";
 import { DisableBtn } from "@/components/Button/DisableBtn";
-import { Dialog } from "@/components/dialog/Dialog";
 import { useRouter } from "next/navigation";
-import Link from "next/link";
+import { AbleBtn } from "@/components/Button/AbleBtn";
 
 const cx = cn.bind(styles);
 
-export interface MyInfoProps {
+export interface CertificationCompleteProps {
     thumbnail?: string;
-    logoutFn: () => void;
-    withdrawFn: () => void;
+
     profileFields: ProfileFields[];
 }
 
@@ -27,25 +25,13 @@ export interface ProfileFields {
     value: string;
 }
 
-const MyInfo = (props: MyInfoProps) => {
-    const {
-        thumbnail: initialThumbnail,
-        profileFields,
-        logoutFn,
-        withdrawFn,
-    } = props;
+const CertificationComplete = (props: CertificationCompleteProps) => {
+    const { thumbnail: initialThumbnail, profileFields } = props;
     /** 썸네일 기본이미지 지정 */
     const [thumbnail, setThumbnail] = useState(
         initialThumbnail || "/mypage/Thumbnail.png"
     );
     const [isModalOpen, setIsModalOpen] = useState(false);
-    const [isDialogOpen, setIsDialogOpen] = useState(false);
-    const [dialog, setDialog] = useState({
-        title: "",
-        content: "",
-        leftButtonText: "",
-        rightButtonText: "",
-    });
 
     /** 유저 정보 필드 */
     const [profile, setProfile] = useState({
@@ -68,30 +54,6 @@ const MyInfo = (props: MyInfoProps) => {
     const closeModal = () => {
         setIsModalOpen(false);
     };
-    /** 로그아웃 ,탈퇴 dialog  */
-    const openDialog = (action: "logout" | "withdraw") => {
-        if (action === "logout") {
-            setDialog({
-                title: "로그아웃",
-                content: "로그아웃하시겠습니까?",
-                leftButtonText: "취소",
-                rightButtonText: "로그아웃",
-            });
-        } else if (action === "withdraw") {
-            setDialog({
-                title: "탈퇴하기",
-                content:
-                    "탈퇴하면 현재 계정으로 작성한 글, 댓글 등을 수정하거나 삭제할 수 없습니다. 지금 탈퇴하시겠습니까?",
-                leftButtonText: "취소",
-                rightButtonText: "탈퇴하기",
-            });
-        }
-        setIsDialogOpen(true);
-    };
-
-    const closeDialog = () => {
-        setIsDialogOpen(false);
-    };
 
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = e.target;
@@ -99,16 +61,6 @@ const MyInfo = (props: MyInfoProps) => {
             ...prevProfile,
             [name]: value,
         }));
-    };
-
-    const handleLogout = () => {
-        setIsDialogOpen(false);
-        logoutFn();
-    };
-
-    const handleWithdraw = () => {
-        setIsDialogOpen(false);
-        withdrawFn();
     };
 
     /** 뒤로가기 */
@@ -133,14 +85,12 @@ const MyInfo = (props: MyInfoProps) => {
                 title={"내정보 수정"}
                 leftIcon={<FaAngleLeft onClick={handleGoBack} />}
             />
-
             <div className={cx("Thumbnail")}>
                 <Image src={thumbnail} alt="Profile" width={68} height={68} />
                 <div className={cx("ThumbnailEdit")} onClick={openModal}>
                     <MdOutlinePhotoCamera />
                 </div>
             </div>
-
             {/* 파일 올리는 기능 구현 필요 */}
             {/* 모달 */}
             {isModalOpen && (
@@ -166,14 +116,9 @@ const MyInfo = (props: MyInfoProps) => {
                     </div>
                 </div>
             )}
-
             <div className={cx("ProfileInfoWrapper")}>
                 <p className={cx("ProfileInfo")}>회원 정보</p>
-                <Link href="/mypage/certification">
-                    <p className={cx("ProfileEdit")}>수정</p>
-                </Link>
             </div>
-
             <div className={cx("ProfileInputWrapper")}>
                 {profileFields.map((field) => (
                     <div key={field.name} className={cx("ProfileInputTitle")}>
@@ -188,31 +133,11 @@ const MyInfo = (props: MyInfoProps) => {
                 ))}
             </div>
 
-            <div className={cx("LogoutWrapper")}>
-                <a href="#" onClick={() => openDialog("logout")}>
-                    로그아웃
-                </a>
-                <a href="#" onClick={() => openDialog("withdraw")}>
-                    회원탈퇴
-                </a>
+            <div className={cx("SumbitBtn")}>
+                <AbleBtn label={"저장하기"} />
             </div>
-
-            {isDialogOpen && (
-                <Dialog
-                    title={dialog.title}
-                    content={dialog.content}
-                    leftButtonText={dialog.leftButtonText}
-                    rightButtonText={dialog.rightButtonText}
-                    onLeftButtonClick={closeDialog}
-                    onRightButtonClick={
-                        dialog.rightButtonText === "로그아웃"
-                            ? handleLogout
-                            : handleWithdraw
-                    }
-                />
-            )}
         </div>
     );
 };
 
-export default MyInfo;
+export default CertificationComplete;
